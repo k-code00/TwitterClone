a70f0f31e17fa349b1d43d1fcf67e2ed5c5cb321
kojo <kojo.abakah@techconsulting.tech>
2023-10-20 09:28:35 -0400

    project completed

---
+++ Q/Q/Utils/Views/LazyView.swift
@@ -0,0 +1,19 @@
+//
+//  LazyView.swift
+//  Q
+//
+//  Created by Consultant on 20/10/2023.
+//
+
+import SwiftUI
+
+struct LazyView<Content: View>: View {
+    let build: () -> Content
+    init(_ build: @autoclosure @escaping() -> Content) {
+        self.build = build
+    }
+    
+    var body: Content {
+        build()
+    }
+}
---
+++ Q/Q/Utils/Views/TextArea.swift
@@ -0,0 +1,33 @@
+//
+//  TextArea.swift
+//  Q
+//
+//  Created by Consultant on 20/10/2023.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    @Binding var text: String
+    let placeholder: String
+    
+    init(_ placeholder: String, text: Binding<String>) {
+        self._text = text
+        self.placeholder = placeholder
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color(.placeholderText))
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 12)
+            }
+            
+            TextEditor(text: $text)
+                .padding(4)
+        }.font(.body)
+    }
+}
